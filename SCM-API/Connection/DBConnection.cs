8c692ab839d431b0f8f6d9c602856335369c1b71
RamonUlises <jorgitomilina@gmail.com>
2024-10-08 21:03:18 -0600

    Agregar archivos de proyecto.

---
+++ SCM-API/Connection/DBConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace SCM_API.Connection
+{
+    public class DBConnection
+    {
+        private readonly string connectionString = "Server=RAMON\\SQLEXPRESS;Database=sistema_gestion_matricula;Integrated Security=True;";
+
+        public SqlConnection AbrirConexion()
+        {
+            SqlConnection conexion = new(connectionString);
+
+            try
+            {
+                conexion.Open();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return conexion;
+        }
+
+        public void CerrarConexion(SqlConnection conexion)
+        {
+            if (conexion != null && conexion.State == System.Data.ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
