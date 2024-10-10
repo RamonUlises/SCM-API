300d5ca0c48a021e7f0c5b17274b4d926f2dc06a
RamonUlises <jorgitomilina@gmail.com>
2024-10-10 15:03:07 -0600

    Actualizar procedimientos y triggers para la tabla tutores_x_estudiantes

--- database/procedures/tutores_x_estudiantes.sql
+++ database/procedures/tutores_x_estudiantes.sql
@@ -1,1 +1,55 @@
 -- procedures para tutores_x_estudiantes
+
+-- crear tabla tutores_x_estudiantes
+CREATE PROCEDURE sp_agregar_tutor_x_estudiante
+  @nombres VARCHAR(255),
+  @apellidos VARCHAR(255),
+  @cedula VARCHAR(16),
+  @telefono VARCHAR(9)
+AS
+BEGIN
+  INSERT INTO tutores_x_estudiantes(nombres, apellidos, cedula, telefono)
+  VALUES(@nombres, @apellidos, @cedula, @telefono)
+END;
+
+-- editar tutor_x_estudiante
+CREATE PROCEDURE sp_editar_tutor_x_estudiante
+@id_tutor_x_estudiante INT,
+@nombres VARCHAR(255),
+@apellidos VARCHAR(255),
+@cedula VARCHAR(16),
+@telefono VARCHAR(9)
+AS
+BEGIN
+  UPDATE tutores_x_estudiantes
+  SET nombres = @nombres,
+  apellidos = @apellidos,
+  cedula = @cedula,
+  telefono = @telefono
+  WHERE id_tutor_x_estudiante = @id_tutor_x_estudiante
+END;
+
+-- borrar tutor_x_estudiante
+CREATE PROCEDURE sp_borrar_tutor_x_estudiante
+@id_tutor_x_estudiante INT
+AS
+BEGIN
+    DELETE FROM tutores_x_estudiantes
+    WHERE id_tutor_x_estudiante = @id_tutor_x_estudiante
+END;
+
+-- obtener todos los tutores_x_estudiantes
+CREATE PROCEDURE sp_obtener_tutores_x_estudiantes
+AS
+BEGIN
+  SELECT * FROM tutores_x_estudiantes
+END;
+
+-- obtener tutor_x_estudiante por id
+CREATE PROCEDURE sp_obtener_tutor_x_estudiante
+@id_tutor_x_estudiante INT
+AS
+BEGIN
+    SELECT * FROM tutores_x_estudiantes
+    WHERE id_tutor_x_estudiante = @id_tutor_x_estudiante
+END;
--- database/triggers/tutores_x_estudiantes.sql
+++ database/triggers/tutores_x_estudiantes.sql
@@ -1,1 +1,29 @@
 -- triggers para tutores_x_estudiantes
+
+CREATE TRIGGER tr_validar_cedula_tutor_x_estudiante
+ON tutores_x_estudiantes
+FOR INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS(
+        SELECT cedula FROM tutores_x_estudiantes
+        WHERE cedula = (SELECT cedula FROM INSERTED)
+    )
+    BEGIN
+        RAISERROR('La cedula ya existe en la base de datos', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+END;
+
+CREATE TRIGGER tr_update_tutores_x_estudiantes
+ON tutores_x_estudiantes
+FOR UPDATE
+AS
+BEGIN
+    UPDATE tutores_x_estudiantes
+    SET nombres = (SELECT nombres FROM INSERTED),
+    apellidos = (SELECT apellidos FROM INSERTED),
+    cedula = (SELECT cedula FROM INSERTED),
+    telefono = (SELECT telefono FROM INSERTED)
+    WHERE id_tutor_x_estudiante = (SELECT id_tutor_x_estudiante FROM INSERTED)
+END;
