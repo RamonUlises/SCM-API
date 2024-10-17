d54d961246c486c1af2fcff777072e0594de3edb
Moises Laguna <lagunamoises64@gmail.com>
2024-10-17 16:58:22 -0600

    La mama de la mama

---
+++ SCM-API/Controllers/DatosAcadeimicos.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SCM_API.Clase;
+using SCM_API.Lib;
+
+namespace SCM_API.Controllers
+{
+    [Route("/api/datos-academicos")]
+    [ApiController]
+    public class DatosAcademicos : Controller
+    {
+        [HttpGet]
+        public IActionResult ObtenerDatosAcademicos()
+        {
+            var datosAcademicos = new Models.DatosAcademicos().ObtenerDatosAcademicos();
+
+            if (datosAcademicos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(datosAcademicos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObtenerDatosAcademicos(int id)
+        {
+            var datosAcademicos = new Models.DatosAcademicos().ObtenerDatosAcademicos(id);
+
+            if (datosAcademicos == null)
+            {
+                return NotFound(new { message = "Datos academicos no encontrados" });
+            }
+
+            return Ok(datosAcademicos);
+        }
+
+        [HttpPost]
+        public IActionResult CrearDatosAcademicos([FromBody] DatosAcademicosClass datosAcademicos)
+        {
+            try
+            {
+                var res = new ValidateDatosAcademicos().ValidarDatosAcademicos(datosAcademicos);
+
+                if (res.status == false)
+                {
+                    return BadRequest(new { res.message });
+                }
+
+                var result = new Models.DatosAcademicos().CrearDatosAcademicos(datosAcademicos);
+                if (result.status == false)
+                {
+                    return BadRequest(new { result.message });
+                }
+
+                return Ok(new { result.message });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult ActualizarDatosAcademicos(int id, [FromBody] DatosAcademicosClass datosAcademicos)
+        {
+            try
+            {
+                var res = new ValidateDatosAcademicos().ValidarDatosAcademicos(datosAcademicos);
+
+                if (res.status == false)
+                {
+                    return BadRequest(new { res.message });
+                }
+
+                var result = new Models.DatosAcademicos().ActualizarDatosAcademicos(id, datosAcademicos);
+                if (result.status == false)
+                {
+                    return BadRequest(new { result.message });
+                }
+
+                return Ok(new { result.message });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EliminarDatosAcademicos(int id)
+        {
+            var result = new Models.DatosAcademicos().EliminarDatosAcademicos(id);
+
+            if (result.status == false)
+            {
+                return BadRequest(new { result.message });
+            }
+
+            return Ok(new { result.message });
+        }
+    }
+}
---
+++ SCM-API/Models/DatosAcademicos.cs
@@ -0,0 +1,45 @@
+﻿using SCM_API.Clase;
+using SCM_API.Connection;
+using SCM_API.Lib;
+using System.Data.SqlClient;
+
+namespace SCM_API.Models
+{
+    public class DatosAcademicos
+    {
+        public List<DatosAcademicosClass>? ObtenerDatosAcademicos()
+        {
+            try
+            {
+                SqlConnection conexion = new DBConnection().AbrirConexion();
+
+                string query = "EXEC sp_obtener_datos_academicos";
+                SqlCommand command = new(query, conexion);
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<DatosAcademicosClass> datosAcademicos = [];
+
+                while (reader.Read())
+                {
+                    DatosAcademicosClass datosAcademico = new()
+                    {
+                        CodigoEstudiante = reader["CodigoEstudiante"].ToString() ?? string.Empty,
+                        FechaMatricula = reader["FechaMatricula"].ToString() ?? string.Empty,
+                        NivelEducativo = reader["NivelEducativo"].ToString() ?? string.Empty,
+                        Repitente = reader["Repitente"].ToString() ?? string.Empty,
+                        Grado = reader["Grado"].ToString() ?? string.Empty,
+                        Seccion = reader["Seccion"].ToString() ?? string.Empty,
+                        Turno = reader["Turno"].ToString() ?? string.Empty,
+                        Centro = reader["Centro"].ToString() ?? string.Empty,
+                        IdEstudiante = reader["IdEstudiante"].ToString() ?? string.Empty
+                    };
+
+                    datosAcademicos.Add(datosAcademico);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Log.WriteLog(ex.Message);
+            }
+        }
+    }
