4e460377013de9b9d4c900745ca58c5caaa83b96
Carlos Figueroa <nicacharle@gmail.com>
2024-10-24 20:03:43 -0600

    TutoresXEstudiantes

---
+++ SCM-API/Controllers/TutoresXEstudiantes.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SCM_API.Clase;
+using SCM_API.Lib;
+using SCM_API.Models;
+
+namespace SCM_API.Controllers
+{
+    public class TutoresXEstudiantes
+    {
+        [Route("/api/tutores-estudiantes")]
+        [ApiController]
+        public class TutoresXEstudiante : Controller
+        {
+            [HttpGet]
+            public IActionResult ObtenerTutoresEstudiantes ()
+            {
+                var tutor = new Models.TutoresEstudiantes().ObtenerTutoresEstudiantes();
+
+                if (tutor == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(tutor);
+            }
+
+            [HttpGet("{id}")]
+            public IActionResult ObtenerTutoresEstudiantes(int id)
+            {
+                var tutor = new Models.TutoresEstudiantes().ObtenerTutoresEstudiantes(id);
+
+                if (tutor == null)
+                {
+                    return NotFound(new { message = "Estudiante no encontrado" });
+                }
+
+                return Ok(tutor);
+            }
+            [HttpPost]
+            public IActionResult CrearTutorEstudiante([FromBody] TutoresXEstudiantesClass tutores)
+            {
+                try
+                {
+
+                    var res = new ValidateTutores().ValidarTutores(tutores);
+
+                    if (res.status == false)
+                    {
+                        return BadRequest(new { res.message });
+                    }
+
+                    var result = new Models.TutoresEstudiantes().CrearTutoresEstudiantes(tutores);
+                    if (result.status == false)
+                    {
+                        return BadRequest(new { result.message });
+                    }
+
+                    return Ok(new { result.message });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { message = ex.Message });
+                }
+            }
+
+            [HttpPut("{id}")]
+            public IActionResult ActualizarTutoresEstudiantes(int id, [FromBody] TutoresXEstudiantesClass tutores)
+            {
+                try
+                {
+                    var res = new ValidateTutores().ValidarTutores(tutores);
+
+                    if (res.status == false)
+                    {
+                        return BadRequest(new { res.message });
+                    }
+
+                    var result = new Models.TutoresEstudiantes().EditarTutoresEstudiantes(id, tutores);
+                    if (result.status == false)
+                    {
+                        return BadRequest(new { result.message });
+                    }
+
+                    return Ok(new { result.message });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { message = ex.Message });
+                }
+            }
+
+            [HttpDelete("{id}")]
+            public IActionResult EliminarTutoresEstudiantes(int id)
+            {
+                var result = new Models.TutoresEstudiantes().EliminarTutoresEstudiantes(id);
+
+                if (result.status == false)
+                {
+                    return BadRequest(new { result.message });
+                }
+
+                return Ok(new { result.message });
+            }
+        
+
+        }
+    }
+}
---
+++ SCM-API/Lib/ValidateTutores.cs
@@ -0,0 +1,34 @@
+﻿using SCM_API.Clase;
+using SCM_API.Models;
+
+namespace SCM_API.Lib
+{
+    public class ValidateTutores
+    {
+        public Errors ValidarTutores(TutoresXEstudiantesClass TutoresEstudiantes)
+        {
+            try
+            {
+                new Validates().String(TutoresEstudiantes.Nombre, "Nombres");
+                new Validates().String(TutoresEstudiantes.Apellido, "Apellidos");
+                new Validates().Cedula(TutoresEstudiantes.Cedula);
+                new Validates().Telefono(TutoresEstudiantes.Telefono);
+
+                //si el id del tutor por estudiante existe entonces no se crea el tutor, si no existe se crea un nuevo tutor por estudiante
+                var tutor = new Models.TutoresEstudiantes().ObtenerTutoresEstudiantes(TutoresEstudiantes.Id_tutor_x_estudiante);
+                if (tutor != null)
+                {
+                    return new Errors { message = "Tutor ya existe", status = false };
+                }
+
+                return new Errors { message = "Tutor creado", status = true };
+
+
+            }
+            catch (Exception ex)
+            {
+                return new Errors { message = ex.Message, status = false };
+            }
+        }
+    }
+}
